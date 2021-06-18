1b65590ed78abf3efe6754c7dd4e48020025cd54
KenanOmeradzic <77675920+KenanOmeradzic@users.noreply.github.com>
2021-06-18 11:21:45 +0200

    Update MainAppl.java
    Add new Player

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,6 +7,7 @@ public class MainAppl {
 	public MainAppl() {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
-	}
+		player = new Player(4, "Hinteregger", "Defense");
+		System.out.println("== " + player);
 
 }
